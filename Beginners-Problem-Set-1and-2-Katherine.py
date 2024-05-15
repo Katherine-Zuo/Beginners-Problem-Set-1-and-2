4ce39e2dd892ad900f53c34b231a74999767d9fa
Katherine-Zuo <161545972+Katherine-Zuo@users.noreply.github.com>
2024-05-15 20:38:26 +1200

    Create Beginners-Problem-Set-1and-2-Katherine.py

---
+++ Beginners-Problem-Set-1and-2-Katherine.py
@@ -0,0 +1,30 @@
+
+name=input ("What is your name? ")
+adj=input ("Enter an adjective: ")
+verb=input ("Enter a verb: ")
+place=input ("Enter a place: ")
+food=input ("Enter a food: ")
+vehicle=input ("Enter a vehicle: ")
+print(name+" is a very "+adj+" person to be around. They love to cook in home. Their favourite food is "+food+" which they eat in a "+vehicle+".")
+
+Convert Minutes into Seconds
+minutes=int(input ("Please input minutes: "))
+second=minutes*60
+print (f"{second} seconds")
+
+ 
+ Birthday Cards
+card=int(input ("How many cards did Cruella get?"))
+Jane=card*2
+print (f"Jane receives at least {Jane}")
+
+Age to days
+age=int(input ("Enter your age in years "))
+day=age*365
+print(f"You are {day} days old)
+"3.1"
+age=int(input ("Enter your age in years "))
+day=age*365
+century=36500-day
+print (f"You are {day} days old. In {century} days you will be 100 years old!")
+
